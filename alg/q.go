d6f9a320e67aa51db65778734e235d193b607252
heptagons <heptagons.protonmail.com>
2023-08-07 16:45:04 -0500

    Q32s.abcdef

--- alg/q.go
+++ alg/q.go
@@ -24,7 +24,9 @@ func (q *Q32) String() string {
 	if n == 1 {
 		// b/a
 		s.WriteString(fmt.Sprintf("%d", b))
-	} else if n == 3 {
+	}
+
+	if n == 3 {
 		// (b + c√d)/a
 		c, d := q.num[1], q.num[2]
 		par := a > 1 && b*c != 0
@@ -35,7 +37,14 @@ func (q *Q32) String() string {
 		if par {
 			s.WriteString(")")
 		}
+	} else if n == 5 {
+		// (b + c√d + e√f)/a
+		c, d, e, f := q.num[1], q.num[2], q.num[3], q.num[4]
+		s.WriteString("(")
+		q.bcdefStr(s, b, c, d, e, f)
+		s.WriteString(")")
 	}
+	// denominator
 	if a > 1 {
 		s.WriteString(fmt.Sprintf("/%d", a))
 	}
@@ -62,6 +71,33 @@ func (q *Q32) bcdStr(s *Str, b, c, d Z32) { // b + c√d
 	s.WriteString(fmt.Sprintf("√%d", d))
 }
 
+func (q *Q32) bcdefStr(s *Str, b, c, d, e, f Z32) { // b + c√d + e√f 
+	if b != 0 {
+		s.WriteString(fmt.Sprintf("%d", b))
+	}
+	if c == 1 {
+		if b != 0 {
+			s.WriteString("+")
+		}
+	} else if c == -1 {
+		s.WriteString("-") // don't put -1
+	} else {
+		if b == 0 {
+			s.WriteString(fmt.Sprintf("%d", c))
+		} else {
+			s.WriteString(fmt.Sprintf("%+d", c))
+		}
+	}
+	s.WriteString(fmt.Sprintf("√%d", d))
+	if e == 1 {
+		s.WriteString("+")
+	} else if e == -1 {
+		s.WriteString("-")
+	} else {
+		s.WriteString(fmt.Sprintf("%+d", e))
+	}
+	s.WriteString(fmt.Sprintf("√%d", f))
+}
 
 
 type Q32s struct {
@@ -84,19 +120,22 @@ func NewQ32s(factory *N32s) *Q32s {
 // ------------=---------------=-------------------------------=---------------------------------------------------------------=------
 //                                                             a
 // newQ32 returns reduced rational number
-func (qs *Q32s) newQ32(den N, num ...Z) (q *Q32, err error) {
+func (qs *Q32s) newQ32(den N, n ...Z) (q *Q32, err error) {
 	if den == 0 {
 		return nil, ErrInfinite
 	} else if den > N32_MAX {
 		return nil, ErrOverflow
 	}
 	a := den
-	switch len(num) {
+	switch len(n) {
 	case 1:
-		return qs.ab(a, num[0])
+		return qs.ab(a, n[0])
 	case 3:
-		//fmt.Println("newQ32 case 3", a, num[0], num[1], num[2])
-		return qs.abcd(a, num[0], num[1], num[2]) // (b+c√d)/a
+		return qs.abcd(a, n[0], n[1], n[2]) // (b+c√d)/a
+	case 5:
+		return qs.abcdef(a, n[0], n[1], n[2], n[3], n[4]) // (b+c√d+e√f)/a
+
+
 	default:
 		return nil, ErrInvalid
 	}
@@ -131,6 +170,42 @@ func (qs *Q32s) abcd(a N, b, c, d Z) (*Q32, error) {
 		}, nil
 	}
 }
+
+// abcdef simplifies and return (b + c√d + e√f)/a
+func (qs *Q32s) abcdef(a N, b, c, d, e, f Z) (*Q32, error) {
+	if e == 0 || f == 0 {
+		return qs.abcd(a, b, c, d) // (b + c√d)/a
+	} else if f == 1 {
+		return qs.abcd(a, b+e, c, d) // (b+e + c√d)/a
+	} else if d == f {
+		//
+		return qs.abcd(a, b, c+e, d) // (b + (c+e)√d)/a
+
+	} else if c32, d32, err := qs.reduceRoot(c, d); err != nil { // c√d
+		return nil, err
+	} else if d32 == 1 {
+		return qs.abcd(a, b+Z(c32), e, f) // (b+c + e√f)/a
+
+	} else if e32, f32, err := qs.reduceRoot(e, f); err != nil { // e√f
+		return nil, err
+	} else if f32 == 1 {
+		return qs.abcd(a, b+Z(e32), Z(c32), Z(d32)) // (b+e, c√d)/a
+	
+	} else if d32 == f32 {
+		return qs.abcd(a, b, Z(c32) + Z(e32), Z(d32)) // (b, (c+e)√d)/a)
+
+	} else if a32, bce32, err := qs.reduceQn(a, b, Z(c32), Z(e32)); err != nil { // (b,c,e)/a
+		return nil, err
+	} else {
+		return &Q32{
+			den: a32, // a
+			num: []Z32{ bce32[0], bce32[1], d32, bce32[2], f32 }, // b,c,d,e,f
+		}, nil
+	}
+}
+
+
+
 
 func (qs *Q32s) AddQ(q ...*Q32) (s *Q32, err error) {
 	n := len(q)
@@ -255,7 +330,7 @@ func (qs *Q32s) addQ2(q, r *Q32) (s *Q32, err error) {
 			}
 		}
 	}
-	return nil, ErrInvalid
+	return nil, fmt.Errorf("Can't sum %s and %s", q.String(), r.String())
 }
 
 
