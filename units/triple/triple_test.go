9fe44ebba52e2708855077ed6b81ce3f507c43bd
heptagons <heptagons.protonmail.com>
2023-09-20 12:53:54 -0500

    tex figure 1 right

--- units/triple/triple_test.go
+++ units/triple/triple_test.go
@@ -7,38 +7,57 @@ import (
 	"github.com/heptagons/meccano"
 )
 
-func Hexagons(max int) *meccano.Sols {
-	sols := &meccano.Sols{}
+type Triples struct {
+	*meccano.Sols
+}
+
+func NewTriples() *Triples {
+	return &Triples{
+		Sols: &meccano.Sols{},
+	}
+}
+
+func (t *Triples) squareRoot(radical int) (int, bool) {
+	if f := float64(radical); f <= 0 {
+		// radical is negative
+		return 0, false
+	} else if e := int(math.Sqrt(f)); math.Pow(float64(e), 2) != f {
+		// e is not square
+		return 0, false
+	} else {
+		return e, true
+	}
+}
+
+func (t *Triples) Hexagons(max int) {
     for a := 1 ; a < max; a++ {
     	for b := 1; b < a; b++ {
-    		abab := (a+b)*(a+b)
+    		ab2 := (a+b)*(a+b)
     		a_b := a - b
     		ab := a*b
         	for c := 0; c < a; c++ {
           		for d := 1; d < a; d++ {
           			if c == b+d {
-          				continue; // trivial ey = 0
+          				continue; // reject trivial e y-coordinate = 0 (e horizontal)
           			}
           			if a - b != d {
           				continue; // reject displaced copies
           			}
           			c_d := c - d
-           			if f := float64(abab + (c_d)*(a_b+c_d) - ab); f <= 0 {
-
-           			} else if e := int(math.Sqrt(f)); math.Pow(float64(e), 2) == f {
-	          			if e > a {
-	          				continue; // reject 
-	          			}
-           				sols.Add(a, b, c, d, e)
+           			if e, ok := t.squareRoot(ab2 + (c_d)*(a_b+c_d) - ab); !ok {
+           				// radical negative or not square
+           			} else if e <= a {
+           				// just "nice" hexagons without e larger than a
+           				t.Add(a, b, c, d, e)
            			}
               	}
             }
         }
     }
-    return sols
 }
 
 func TestHexagons(t *testing.T) {
-	sols := Hexagons(60)
-	_ = sols
+	tri := NewTriples()
+	tri.Hexagons(60)
+	//_ = sols
 }
