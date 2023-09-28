1db8cf402f2c116fd0dc373797e3dae65c34683f
heptagons <heptagons.protonmail.com>
2023-09-28 14:32:00 -0500

    folder fox

---
+++ units/fox/fox.go
@@ -0,0 +1,136 @@
+package fox
+
+import (
+	"fmt"
+	. "github.com/heptagons/meccano/nest"
+)
+
+type ABCD struct {
+	a [][]N32
+}
+
+func (e *ABCD) append(a, b, c, d N32) {
+	if e.a == nil {
+		e.a = make([][]N32, 0)
+	}
+	e.a = append(e.a, []N32{ a,b,c,d })
+}
+
+func (abcd *ABCD) print() {
+	for _, a := range abcd.a {
+		fmt.Printf("\t%v\n", a)
+	}
+}
+
+// Fox iterate a,b,c,d until max an reports a,b,c,d and
+//	                    ,---------------------
+//	       -a(2b+c) +- √ a²c² + 4b(b+c)(d²-c²)
+//	cos = ------------------------------------
+//	                    4b(b+c)
+func Fox(max N32, found func(a, b, c, d N32, cos *A32)) {
+	factory := NewA32s()
+	n1 := N32(1)
+	for a := n1; a <= max; a++ {
+		for b := n1; b <= max; b++ {
+			ab := NatGCD(a, b)
+			for c := n1; c <= max; c++ {
+				abc := NatGCD(ab, c)
+				na := N32(4)*b*(b+c)        // 4b(b+c)
+				zb := -Z(a)*(2*Z(b) + Z(c)) // -a(2b+c)
+				zc := Z(1)                  // 1
+				a2c2 := Z(a*a)*Z(c*c)       // a
+				for d := c; d <= max; d++ { // d >= c always
+					if g := NatGCD(abc, d); g > 1 {
+						continue // skip scale repetitions, eg. [1,2,3,4] = [2,4,6,8]
+					}
+					if zd := a2c2 + 4*Z(b)*Z(b+c)*(Z(d*d) - Z(c*c)); zd < 0 {
+						// skip imaginary numbers invalid fox-face, like d too short
+					} else if cos, err := factory.ANew3(N(na), zb, zc, zd); err != nil {
+						// silent overflow
+					} else {
+						found(a, b, c, d, cos)
+					}
+				}
+			}
+		}
+	}
+}
+
+// Fox iterate a,b,c,√d until max an reports a,b,c,√d and
+//	                    ,---------------------
+//	       -a(2b+c) +- √ a²c² + 4b(b+c)(d-c²)
+//	cos = ------------------------------------
+//	                    4b(b+c)
+func FoxSurd(max N32, found func(a, b, c, d N32, cos *A32)) {
+	factory := NewA32s()
+	n1 := N32(1)
+	for a := n1; a <= max; a++ {
+		for b := n1; b <= max; b++ {
+			ab := NatGCD(a, b)
+			for c := n1; c <= max; c++ {
+				abc := NatGCD(ab, c)
+				na := N32(4)*b*(b+c)        // 4b(b+c)
+				zb := -Z(a)*(2*Z(b) + Z(c)) // -a(2b+c)
+				zc := Z(1)                  // 1
+				a2c2 := Z(a*a)*Z(c*c)       // a
+				for surdD := c*c; surdD <= max*max; surdD++ { // surdD >= c*c always
+					if g := NatGCD(abc, surdD); g > 1 {
+						continue // skip scale repetitions, eg. [1,2,3,4] = [2,4,6,8]
+					}
+					if zd := a2c2 + 4*Z(b)*Z(b+c)*(Z(surdD) - Z(c*c)); zd < 0 {
+						// skip imaginary numbers invalid fox-face, like d too short
+					} else if cos, err := factory.ANew3(N(na), zb, zc, zd); err != nil {
+						// silent overflow
+					} else {
+						found(a, b, c, surdD, cos)
+					}
+				}
+			}
+		}
+	}
+}
+
+func FoxTriangles(surd Z) {
+	factory := NewA32s()
+	min := N32(1)
+	max := N32(20)
+	for a := min; a <= max; a++ {
+		for b := min; b <= max; b++ {
+			_2ab := N(2)*N(a)*N(b)
+			aa_bb := Z(a*a) + Z(b*b)
+			for c := min; c <= max; c++ {
+
+				if a + b < c || b + c < a || c + a < b {
+					continue
+				}
+				if cos, err := factory.ANew1(_2ab, aa_bb - Z(c*c)); err != nil {
+					// silent
+				} else {
+					for d := N32(0); d <= max; d++ {
+						for e := N32(0); e <= max; e++ {
+							if m, err := factory.ANew1(N(1), Z(-2)*Z(a+d)*Z(b+e)); err != nil {
+								// silent
+							} else if prod, err := factory.AMulN(cos, m); err != nil {
+								// silent
+							} else if f, ok := prod.IsInteger(); !ok {
+								// silent
+							} else if g := Z(a+d)*Z(a+d) + Z(b+e)*Z(b+e) + Z(f); g == surd {
+
+								if d == 0 || e == 0 {
+									// triangle extension with only 4 bolts not 5
+									fmt.Printf("a=%d b=%d c=%d d=%d e=%d g=sqrt(%d)\n", a, b, c, d, e, g)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
---
+++ units/fox/fox_test.go
@@ -0,0 +1,181 @@
+package fox
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/heptagons/meccano/nest"
+)
+
+
+func TestFoxAll(t *testing.T) {
+	max := N32(22)
+	m := make(map[string]*ABCD, 0)
+	i := 1
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if coss := cos.String(); coss != "0" && coss != "-1" && coss != "1" {
+			// reject uninteresting cos=0,-1,+1
+			if _, ok := m[coss]; !ok {
+				m[coss] = &ABCD{}
+			}
+			m[coss].append(a, b, c, d)
+			i++
+		}
+	})
+	fmt.Printf("max=%d abcd's=%d diff cosines=%d\n", max, i, len(m))
+	for _, coss := range []string {
+		"√3/2",      // cos 30°
+		"√2/2",      // cos 45°
+		"1/2",       // cos 60°
+		"(-1+√5)/4", // cos 72°
+	} {
+		fmt.Printf("arcos(%s)\n", coss)
+		fmt.Printf("\t%v\n", m[coss])
+	}
+}
+
+// max=100, pentagons=1 last=[3,4,8,11] time=399 seconds
+func TestFoxPentagons(t *testing.T) {
+	max := N32(11)
+	fmt.Printf("max-lenght=%d a,b,c,d pentagons:\n", max)
+	i := 0
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(4, -1, 1, 5) { //  cos 72°
+			i++
+			fmt.Printf("% 3d %d,%d,%d,%d\n", i, a, b, c, d)
+		}
+	})
+}
+
+func TestFoxHexagons(t *testing.T) {
+	max := N32(40)
+	fmt.Printf("max-lenght=%d a,b,c,d efficient hexagons:\n", max)
+	i := 0
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(2,1) { //  cos 60°
+			// Efficient hexagons are those when a > b+c
+			if a >= b+c {
+				i++
+				fmt.Printf("% 3d %d,%d,%d,%d\n", i, a, b, c, d)
+			}
+		}
+	})
+}
+// max=40   found=42 last=[32,1,7,37] time=2.7 seconds
+// max=100, found=350 last=[84,1,11,91] time=399 seconds
+
+func TestFoxOctagons(t *testing.T) {
+	max := N32(80)
+	fmt.Printf("max-lenght=%d a,b,c,d octagons:\n", max)
+	i := 0
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(2,0,1,2) { //  cos 45 degrees sqrt{2}/2
+			i++
+			fmt.Printf("% 3d %d,%d,%d,%d\n", i, a, b, c, d)
+		}
+	})
+}
+// === RUN   TestFoxOctagons
+// max-lenght=80 a,b,c,d octagons:
+// NO SOLUTIONS
+
+func TestFoxDecagons(t *testing.T) {
+	max := N32(80)
+	fmt.Printf("max-lenght=%d a,b,c,d decagons:\n", max)
+	i := 0
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(4, 1, 1, 5) { //  cos 36°
+			i++
+			fmt.Printf("% 3d %d,%d,%d,%d\n", i, a, b, c, d)
+		}
+	})
+}
+// max-lenght=80 a,b,c,d decagons:
+// --- PASS: TestFoxDecagons (118.12s)
+// NO SOLUTIONS
+
+func TestFoxDodecagons(t *testing.T) {
+	max := N32(80)
+	fmt.Printf("max-lenght=%d a,b,c,d dodecagons:\n", max)
+	i := 0
+	Fox(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(2,0,1,3) { //  cos 30 degrees sqrt{3}/2
+			i++
+			fmt.Printf("% 3d %d,%d,%d,%d\n", i, a, b, c, d)
+		}
+	})
+}
+// max=80 dodecagons=none, time=116 seconds
+
+
+
+
+// max=100, pentagons=1 last=[3,4,8,11] time=399 seconds
+func TestFoxSurdPentagons(t *testing.T) {
+	max := N32(50)
+	fmt.Printf("max-lenght=%d a,b,c,surdD pentagons:\n", max)
+	i := 0
+	FoxSurd(max, func(a, b, c, surdD N32, cos *A32) {
+		if cos.Equals(4, -1, 1, 5) { //  cos 72°
+			i++
+			fmt.Printf("% 3d) %d,%d,%d,sqrt(%d)\n", i, a, b, c, surdD)
+		}
+	})
+}
+/* SOLUTIONS!!!
+=== RUN   TestFoxDsurdPentagons
+max-lenght=40 a,b,c,surdD pentagons:
+  1) 2,3,3,sqrt(31)
+  2) 3,4,8,sqrt(121) <- skip this is d*d=11 (original solution)
+  3) 4,5,15,sqrt(341)
+  4) 5,6,24,sqrt(781)
+  5) 6,7,35,sqrt(1555)
+  6) 6,10,5,sqrt(211)
+  7) 10,14,21,sqrt(1031)
+  8) 12,21,7,sqrt(781)
+  9) 15,24,16,sqrt(1441)
+--- PASS: TestFoxDsurdPentagons (155.52s)
+*/
+// One more when max=50
+// 10) 20,36,9,sqrt(2101)
+// panic: test timed out after 10m0s
+
+
+
+func TestFoxSurdOctagons(t *testing.T) {
+	max := N32(80)
+	fmt.Printf("max-lenght=%d a,b,c,surdD octagons:\n", max)
+	i := 0
+	FoxSurd(max, func(a, b, c, d N32, cos *A32) {
+		if cos.Equals(2,0,1,2) { //  cos 45 degrees sqrt{2}/2
+			i++
+			fmt.Printf("% 3d) %d,%d,%d,sqrt(%d)\n", i, a, b, c, d)
+		}
+	})
+}
+// === RUN   TestFoxDsurdOctagons
+// max-lenght=80 a,b,c,d octagons:
+// panic: test timed out after 10m0s
+// NO SOLUTIONS
+
+
+
+func TestFoxSurdDecagons(t *testing.T) {
+	max := N32(40)
+	fmt.Printf("max-lenght=%d a,b,c,surdD decagons:\n", max)
+	i := 0
+	FoxSurd(max, func(a, b, c, surdD N32, cos *A32) {
+		if cos.Equals(4, 1, 1, 5) { //  cos 36°
+			i++
+			fmt.Printf("% 3d) %d,%d,%d,sqrt(%d)\n", i, a, b, c, surdD)
+		}
+	})
+}
+// max-lenght=40 a,b,c,surdD decagons:
+// --- PASS: TestFoxDsurdDecagons (157.71s)
+// NO SOLUTIONS
+
+
+func TestFoxTriangles(t *testing.T) {
+	FoxTriangles(221)
+}
