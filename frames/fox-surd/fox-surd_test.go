1aa87434343353d36ddb676023dfeb116936b8a8
heptagons <heptagons.protonmail.com>
2023-10-18 22:34:11 -0500

    TestFoxSurdPenta ok

--- frames/fox-surd/fox-surd_test.go
+++ frames/fox-surd/fox-surd_test.go
@@ -8,22 +8,22 @@ import (
 
 func TestFoxSurdPenta(t *testing.T) {
 
-	max := Z(6)
-	for n := Z(6); n <= max; n++ {
+	max := Z(36)
+	for n := Z(1); n <= max; n++ {
 		for i := Z(0); i <= n; i++ {
-			ii := i*i
-			n5i := n - 5*i
-			n5i2 := n5i*n5i
+			xi := n - 2*i
 			for j := Z(0); j <= n; j++ {
-				jj := j*j
-				_16jjii :=  16*(jj - ii)
-				u := n5i2 + _16jjii + 5 -ii
 
-				v := 2*((2*i-1)*(2*i-1) - (n-1)*(i-1))
+				u := 4*j*j + xi*xi + 5*n*n + xi*j - 5*n*j
+				v := 2*n*xi - xi*j + n*j
 
-				//if v == 0 {
-					fmt.Printf("n=%d i=%d j=%d u=%d v=%d\n", n, i, j, u, v)
-				//}
+				if v == 0 {
+					if n == i && n == j {
+
+					} else {
+						fmt.Printf("n=%d i=%d j=%d u=%d v=%d\n", n, i, j, u, v)
+					}
+				}
 			}
 		}
 	}
