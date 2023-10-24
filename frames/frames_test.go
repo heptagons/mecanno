5baec7f0dd9eb7f6cfc809b08afeb589c12f6b21
heptagons <heptagons.protonmail.com>
2023-10-24 13:55:29 -0500

    meccanoframefive

--- frames/frames_test.go
+++ frames/frames_test.go
@@ -11,8 +11,8 @@ import (
 // ~/github.com/heptagons/meccano$ go test ./frames/. -run TestFramesTriangleSurds -v -count 1
 
 func TestFramesTriangleSurds(t *testing.T) {
-	surd := Z(13)
-	max := N32(10)
+	surd := Z(4*8)
+	max := N32(15)
 	n := 0
 	fmt.Printf("NewFrames().TriangleSurds surd=%d max=%d\n", surd, max)
 	NewFrames().TriangleSurds(surd, max, func(frame *Triangle) {
