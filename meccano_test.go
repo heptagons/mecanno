ccc8a0bfeee993ac84706886d7a6cf5b1eee16cf
heptagons <heptagons.protonmail.com>
2023-11-04 17:02:14 -0600

    TestTri2 46,8

--- meccano_test.go
+++ meccano_test.go
@@ -58,15 +58,15 @@ func TestPythagorasHalf(t *testing.T) {
 }
 
 
-//              (D)     x = b^2 - 5a^2    5a^2 < b^2 < 25a^2
+//              (D)     x = b^2 - r^2s    r^2s < b^2 < r^2s^2
 //               |
-//               | c    AB = a√5
+//               | y    AB = r√s
 //          √x   |      BC = √x
 //      (B)-----(C)     AC = b
-//       |     _/       CD = c
-//   a√5 |   _/         AD = √( (√x)^2 + (a√5 + c)^2 )
-//       | _/   b          = √( x + 5a^2 + c^2 + 2ac√5 )
-//      (A)                = √(u + v√5)
+//       |     _/       CD = y
+//   r√s |   _/         AD = √( (√x)^2 + (r√s + y)^2 )
+//       | _/   b          = √( x + r^2s + y^2 + 2yr√s )
+//      (A)                = √(u + v√s)
 func TestNest5Simple(t *testing.T) {
 	factory := NewA32s()
 	A := N(1)
@@ -74,17 +74,17 @@ func TestNest5Simple(t *testing.T) {
 	C := Z(0)
 	D := Z(1)
 	E := Z(1)
-	for a := 1; a <= 20; a++ {
-		for b := 1; b < 10*a; b++ {
-			if x := b*b - 5*a*a; x > 0 {
-				for c := 1; c <= 20; c++ {
-					F := Z(x) + Z(5*a*a) + Z(c*c)
-					G := Z(2*a*c)
-					H := Z(5)
+	H := Z(5) // for pentagons
+	for r := Z(1); r <= 20; r++ {
+		for b := Z(1); b < 10*r; b++ {
+			if x := b*b - r*r*H; x > 0 {
+				for y := Z(1); y <= 20; y++ {
+					F := Z(x) + Z(r*r)*H + Z(y*y)
+					G := Z(2*r*y)
 					//	(B + C√D + E√(F+G√H)) / A
 					if s, err := factory.ANew7(A, B, C, D, E, F, G, H); err == nil {
 						if s.IsNest(25, 10) {
-							fmt.Printf("a=% 2d b=% 2d x=% 2d c=% 2d s=%v\n", a, b, x, c, s)
+							fmt.Printf("r=%d√%d b=%d x=√%d y=%d s=%v\n", r, H, b, x, y, s)
 						}
 					}
 				}
@@ -92,12 +92,33 @@ func TestNest5Simple(t *testing.T) {
 		}
 	}
 }
-/* Resolves: s.IsNest(25,10):
-a= 5 b= 12 x= 19 c= 9 s=3√(25+10√5)
-a= 5 b= 12 x= 19 c= 16 s=4√(25+10√5)
-a= 10 b= 24 x= 76 c= 18 s=6√(25+10√5)
+// Can't solve: √(5+2√5), √(10+3√5)
+
+/* Solves pentagon's r
+r=5√5 b=12 x=√19 y=9 s=3√(25+10√5)
+r=5√5 b=12 x=√19 y=16 s=4√(25+10√5)
+r=10√5 b=24 x=√76 y=18 s=6√(25+10√5)
 */
 
+/* Solves pentagon's R
+r=1√5 b=5 x=√20 y=5 s=√(50+10√5)
+r=2√5 b=10 x=√80 y=10 s=2√(50+10√5)
+r=3√5 b=15 x=√180 y=15 s=3√(50+10√5)
+r=4√5 b=20 x=√320 y=20 s=4√(50+10√5)
+*/
+
+
+
+//                 (D)       AB = r√s
+//                  |        BC = √x
+//                  | y      CD = y
+//                  |              ____________________
+//      (B)--------(C)       AD = √ (√x)^2 + (r√s+y)^2
+//       |    √x                   ________________________
+//  r√s  |                      = √ x + r^2s + y^2 + 2ry√s
+//       |
+//      (A)
+//
 func TestNest5Complex(t *testing.T) {
 	factory := NewA32s()
 	A := N(1)
@@ -105,16 +126,16 @@ func TestNest5Complex(t *testing.T) {
 	C := Z(0)
 	D := Z(1)
 	E := Z(1)
-	for a := 1; a <= 20; a++ { // √5 - 20√5
+	H := Z(5) // √s for pentagons
+	for r := 1; r <= 20; r++ { // √5 - 20√5
 		for x := 1; x < 200; x++ { // horizontal
-			for c := 1; c < 100; c++ { // vertical
-				F := Z(x) + Z(5*a*a) + Z(c*c)
-				G := Z(2*a*c)
-				H := Z(5)
+			for y := 1; y < 100; y++ { // vertical
+				F := Z(x) + Z(r*r)*H + Z(y*y)
+				G := Z(2*r*y)
 				//	(B + C√D + E√(F+G√H)) / A
 				if s, err := factory.ANew7(A, B, C, D, E, F, G, H); err == nil {
 					if s.IsNest(10,3) {
-						fmt.Printf("a=% 2d x=% 2d c=% 2d s=%v\n", a, x, c, s)
+						fmt.Printf("r=%d√%d x=√%d y=%d s=%v\n", r, H, x, y, s)
 					}
 				}
 			}
@@ -148,3 +169,42 @@ a= 4 x= 95 c= 5 s=2√(50+10√5)
 a= 5 x= 59 c= 4 s=2√(50+10√5)
 a= 9 x= 20 c= 5 s=3√(50+10√5)
 */
+
+
+
+
+//                      (F)        x  = AB
+//                       |        r√s = BD
+//          (C)_       _(E)         y = DF
+//           |  -_   _-  |               __________________________
+//           |   _(D)_   |         AF = √ x^2 + r^2s + y^2 + 2rx√s 
+//           | _-     -_ |
+//  (A)-----(B)         (D)
+//
+func TestNestSix(t *testing.T) {
+	factory := NewA32s()
+	A := N(1)
+	B := Z(0)
+	C := Z(0)
+	D := Z(1)
+	E := Z(1)
+	H := Z(5) // √s for pentagons
+	for r := 1; r <= 50; r++ {
+		for x := 1; x <= 50; x++ {
+			G := Z(2*r*x) // 2rx
+			for y := 1; y <= 50; y++ {
+				F := Z(x*x) + Z(r)*Z(r)*H + Z(y*y) // x^2 + r^2s + y^2
+				//	(B + C√D + E√(F+G√H)) / A
+				if s, err := factory.ANew7(A, B, C, D, E, F, G, H); err == nil {
+					if s.IsNest(10,2) {
+						fmt.Printf("% 2d√%d x=% 2d y=% 2d s=%v\n", r, H, x, y, s)
+					}
+				}
+			}
+		}
+	}
+}
+// Simpler TestNestSix: Can't solve √(5+2√5), √(25+10√5), √(50+10√5)
+// Can't solve: √(10+3√5), √(46+18√5)
+// even with r <= 50, x <=50, y <= 50
+// But solves several: √(10+2√5)
